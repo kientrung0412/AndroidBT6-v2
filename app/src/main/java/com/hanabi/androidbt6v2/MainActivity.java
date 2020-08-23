ff1c13c37d266c3156d37434068357b7dee3f2d3
Hanabi <nguyenkientrung0412@gmail.com>
2020-08-23 09:14:02 +0700

    chỉnh sửa con vòng đời

--- app/src/main/java/com/hanabi/androidbt6v2/MainActivity.java
+++ app/src/main/java/com/hanabi/androidbt6v2/MainActivity.java
@@ -2,36 +2,33 @@ package com.hanabi.androidbt6v2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
+
+import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private int[] arrIdImg = new int[]{
-            R.drawable.aomua, R.drawable.baocao, R.drawable.xedapdien,
-            R.drawable.xakep, R.drawable.vuonbachthu, R.drawable.xaphong,
-            R.drawable.vuaphaluoi, R.drawable.tranhthu, R.drawable.totien,
-            R.drawable.tohoai, R.drawable.tichphan, R.drawable.thothe
-    };
-
+    private TypedArray arrIdImg;
     private String nameImg, result = "";
     private ImageView imgvPlay;
     private Random random = new Random();
-    private int idImg, score, heart = 5, count;
+    private int idImg = -1, score, heart, count;
     private TextView tvScore, tvHeart;
     private GridLayout glSelect, glShows;
     private int[] arrIdBtnShow;
@@ -45,43 +42,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void initViews() {
-        //Lấy ảnh ngẫu nhiên
-        createPlay();
-
         //mapping đến view
+        arrIdImg = getResources().obtainTypedArray(R.array.images);
         tvHeart = findViewById(R.id.tv_heart);
         tvScore = findViewById(R.id.tv_score);
-        tvScore.setText(score + "");
-        tvHeart.setText(heart + "");
         glShows = findViewById(R.id.gr_btn_show);
         glSelect = findViewById(R.id.gr_btn_select);
         imgvPlay = findViewById(R.id.imgv_play);
         btnContinue = findViewById(R.id.btn_continue);
 
-        idImg = arrIdImg[idImg];
-
-        //set ảnh
-        imgvPlay.setImageResource(idImg);
-        //lấy tên ảnh
-        nameImg = this.getResources().getResourceEntryName(idImg);
-        arrIdBtnShow = new int[nameImg.length()];
-        //Hiển thị số ô chữ
-        for (int i = 0; i < nameImg.length(); i++) {
-            setStyleTwShow(i);
-        }
-
-        //String to ArrayList
-        String[] charArray = nameImg.split("");
-        ArrayList<String> chars = new ArrayList<>(Arrays.asList(charArray));
-        chars.remove(0);
-        Collections.shuffle(chars);
-        chars = chars;
-        //Hiển thị ô trọn chữa
-        for (int i = 0; i < 12; i++) {
-            setStyleTwSelect(i, chars);
-        }
-        btnContinue.setVisibility(View.INVISIBLE);
-        btnContinue.setOnClickListener(this);
+        reStartGame();
 
     }
 
@@ -90,43 +60,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         result = "";
         glSelect.removeAllViews();
         glShows.removeAllViews();
-        initViews();
+        btnContinue.setVisibility(View.INVISIBLE);
+        btnContinue.setOnClickListener(this);
+        createPlay();
     }
 
     private void reStartGame() {
         heart = 5;
         score = 0;
+        tvScore.setText(score + "");
+        tvHeart.setText(heart + "");
         continueGame();
     }
 
-    private void setStyleTwSelect(int i, ArrayList<String> chars) {
-        TextView textView = new TextView(glSelect.getContext());
-        textView.setBackgroundResource(R.drawable.ic_tile_hover);
+    private void setStyleTvSelect(int i, ArrayList<String> chars) {
+        TextView textView = new TextView(this);
         if (i < nameImg.length()) {
             textView.setText(chars.get(i));
         } else {
             char character = (char) (random.nextInt(25) + 97);
             textView.setText(character + "");
         }
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextSize(24);
-        textView.setTextColor(Color.WHITE);
+        setStyleTv(textView, R.drawable.ic_tile_hover);
         textView.setOnClickListener(this);
         //Thêm nút
         glSelect.addView(textView);
     }
 
-    private void setStyleTwShow(int i) {
+    private void setStyleTvShow(int i) {
         //tạo nút mới và xác định vị trí thuộc vào
-        TextView textView = new TextView(glShows.getContext());
-        textView.setBackgroundResource(R.drawable.ic_tile_hover);
-        textView.setTextSize(24);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(Color.WHITE);
+        TextView textView = new TextView(this);
+        setStyleTv(textView, R.drawable.ic_tile_hover);
         textView.setId(9999900 + i);
         arrIdBtnShow[i] = textView.getId();
         //Thêm nút
         glShows.addView(textView);
+    }
+
+    private void setStyleTv(TextView view, int background) {
+        view.setGravity(Gravity.CENTER);
+        view.setTextSize(24);
+        view.setTextColor(Color.WHITE);
+        view.setBackgroundResource(background);
     }
 
     @Override
@@ -137,13 +112,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             default:
                 if (count < nameImg.length()) {
-                    Toast.makeText(this, view.getId() + "", Toast.LENGTH_SHORT).show();
                     TextView textView = (TextView) view;
                     textView.setVisibility(View.INVISIBLE);
                     String character = textView.getText().toString();
                     result += character;
-                    TextView textView1 = findViewById(arrIdBtnShow[count]);
-                    textView1.setText(character);
+                    ((TextView) findViewById(arrIdBtnShow[count])).setText(character);
                     count++;
                     //kiem tra thang thua
                     if (count == nameImg.length()) {
@@ -155,7 +128,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void isWin() {
-        int idBg = 0;
+        int idBg;
         if (nameImg.equalsIgnoreCase(result)) {
             idBg = R.drawable.ic_tile_true;
             score += 100;
@@ -170,20 +143,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             tvHeart.setText(heart + "");
         }
         for (int id : arrIdBtnShow) {
-            TextView textView = findViewById(id);
-            textView.setBackgroundResource(idBg);
+            findViewById(id).setBackgroundResource(idBg);
         }
-
         btnContinue.setVisibility(View.VISIBLE);
     }
 
     private void createPlay() {
-        int id = random.nextInt(arrIdImg.length);
+        int id = random.nextInt(arrIdImg.length());
         while (idImg == id) {
-            id = random.nextInt(arrIdImg.length);
+            id = random.nextInt(arrIdImg.length());
         }
-
         idImg = id;
+        idImg = arrIdImg.getResourceId(idImg, -1);
+        //set anh
+        Glide.with(this).load(idImg).into(imgvPlay);
+        //lấy tên ảnh
+        nameImg = getResources().getResourceEntryName(idImg);
+        //thiết lập số ô điền
+        arrIdBtnShow = new int[nameImg.length()];
+        //Hiển thị số ô chữ
+        for (int i = 0; i < nameImg.length(); i++) {
+            setStyleTvShow(i);
+        }
+        //String to ArrayList
+        String[] charArray = nameImg.split("");
+        ArrayList<String> chars = new ArrayList<>(Arrays.asList(charArray));
+        chars.remove(0);
+        // sort ramdom
+        Collections.shuffle(chars);
+        //Hiển thị ô trọn chữ
+        for (int i = 0; i < 12; i++) {
+            setStyleTvSelect(i, chars);
+        }
     }
 
     @Override
